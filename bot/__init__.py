6071082e854a3b22474329e97d8701214fd8cb14
thetoonswood <118585979+thetoonswood@users.noreply.github.com>
2022-12-08 09:26:45 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -43,6 +43,22 @@ qbit_options = {}
 TIME_GAP_STORE = {}
 GLOBAL_EXTENSION_FILTER = ['.aria2']
 
+CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
+try:
+    if len(CONFIG_FILE_URL) == 0:
+        raise TypeError
+    try:
+        res = rget(CONFIG_FILE_URL)
+        if res.status_code == 200:
+            with open('config.env', 'wb+') as f:
+                f.write(res.content)
+        else:
+            log_error(f"Failed to download config.env {res.status_code}")
+    except Exception as e:
+        log_error(f"CONFIG_FILE_URL: {e}")
+except:
+    pass
+  
 try:
     if bool(environ.get('_____REMOVE_THIS_LINE_____')):
         log_error('The README.md file there to be read! Exiting now!')
